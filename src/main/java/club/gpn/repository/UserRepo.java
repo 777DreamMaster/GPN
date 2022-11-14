cb07cf779921e8ca5f3a58680be02e041ad21590
Alexei <chumakovalexei@mail.ru>
2022-11-14 18:35:25 +0300

    User Repo

---
+++ src/main/java/club/gpn/repository/UserRepo.java
@@ -0,0 +1,12 @@
+package club.gpn.repository;
+
+import club.gpn.model.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepo extends CrudRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+}
